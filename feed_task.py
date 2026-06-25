7c00cc2efe0fe8cef85a3da5086dcd56da1b1a16
YóUnǎi <26835631+ames0k0@users.noreply.github.com>
2026-06-25 13:59:06 +0300

    core: Added functions to filter products and categories

--- feed_task.py
+++ feed_task.py
@@ -1,6 +1,143 @@
 from datetime import datetime
 
 
+def filter_active_products(products_stream):
+    for product in products_stream:
+        if not product["is_active"]:
+            continue
+        yield product
+
+
+def filter_products_with_name(products_stream):
+    for product in products_stream:
+        if not product["name"]:
+            continue
+        if not product["name"].strip():
+            continue
+        yield product
+
+
+def filter_products_with_picture(products_stream):
+    for product in products_stream:
+        if not product["image_url"]:
+            continue
+        if not (
+            product["image_url"].strip().startswith("http://")
+            or
+            product["image_url"].strip().startswith("https://")
+        ):
+            continue
+        yield product
+
+
+def filter_products_with_price(products_stream, gt: float = 0.0):
+    for product in products_stream:
+        price = product["price"]
+        if not price:
+            continue
+        price = float(price.strip())
+        if price <= gt:
+            continue
+        yield product
+
+
+def filter_products_with_active_category(products_stream, categories):
+    for product in products_stream:
+        product_category = None
+        for category in categories:
+            if product["category_id"] == category["id"]:
+                product_category = category
+                break
+        if not product_category:
+            continue
+        if not product_category["is_active"]:
+            continue
+        yield product
+
+
+def filter_categories_by_products(products_stream, categories):
+    product_categories_id = set()
+    for product in products_stream:
+        product_categories_id.add(product["category_id"])
+    for category in categories:
+        if category["id"] not in product_categories_id:
+            continue
+        yield category
+
+
+def build_yml(products, categories, generated_at) -> str:
+    xml = '<?xml version="1.0" encoding="UTF-8"?>'
+
+    xml += f'<yml_catalog date="{generated_at}">'
+    xml += "<shop>"
+
+    xml += "<name>Test Shop</name>"
+    xml += "<company>Test Company</company>"
+    xml += "<url>https://example.test</url>"
+
+    xml += '<currencies><currency id="RUB" rate="1"/></currencies>'
+
+    xml += "<categories>"
+
+    # ...
+    products_stream = filter_active_products(products_stream=products)
+    products_stream = filter_products_with_name(products_stream=products_stream)
+    products_stream = filter_products_with_picture(products_stream=products_stream)
+    products_stream = filter_products_with_price(products_stream=products_stream)
+    products_stream = filter_products_with_active_category(
+        products_stream=products_stream,
+        categories=categories,
+    )
+
+    # TODO: sort_by_id
+    products_stream = list(products_stream)
+
+    # TODO: sort_by_id
+    categories_stream = filter_categories_by_products(
+        products_stream=products_stream,
+        categories=categories,
+    )
+    for category in categories_stream:
+        xml += f'<category id="{category["id"]}">{category["name"]}</category>'
+
+    xml += "</categories>"
+    xml += "<offers>"
+
+    for product in products_stream:
+        print(product["id"], end=' ')
+
+        xml += f'<offer id="{product["id"]}" available="{product["stock"]}">'
+
+        xml += f"<url>https://example.test/products/{product['slug']}/</url>"
+
+        price = product["price"].replace(".", ",")
+
+        xml += f"<price>{price}</price>"
+
+        # TODO: document_builder.add_old_price ??
+        if product["old_price"]:
+            if float(product["old_price"]) != 0.0:
+                if float(product["old_price"]) > float(product["price"]):
+                    xml += f"<oldprice>{product['old_price']}</oldprice>"
+                    print(product["price"], "::", product["old_price"])
+
+        xml += "<currencyId>RUB</currencyId>"
+        xml += f"<categoryId>{product['category_id']}</categoryId>"
+        xml += f"<picture>{product['image_url']}</picture>"
+        xml += f"<name>{product['name']}</name>"
+        if product["description"]:
+            xml += f"<description>{product['description']}</description>"
+        else:
+            print(">>NODESC>>", product["id"])
+        xml += "</offer>"
+
+    xml += "</offers>"
+    xml += "</shop>"
+    xml += "</yml_catalog>"
+
+    return xml
+
+
 CATEGORIES = [
     {
         "id": 1,
@@ -108,95 +245,10 @@ PRODUCTS = [
 ]
 
 
-def build_yml(products, categories, generated_at) -> str:
-    xml = '<?xml version="1.0" encoding="UTF-8"?>'
-
-    xml += f'<yml_catalog date="{generated_at}">'
-    xml += "<shop>"
-
-    xml += "<name>Test Shop</name>"
-    xml += "<company>Test Company</company>"
-    xml += "<url>https://example.test</url>"
-
-    xml += '<currencies><currency id="RUB" rate="1"/></currencies>'
-
-    xml += "<categories>"
-
-    for product in products:
-        category = next(
-            category
-            for category in categories
-            if category["id"] == product["category_id"]
-        )
-
-        xml += f'<category id="{category["id"]}">{category["name"]}</category>'
-
-    xml += "</categories>"
-    xml += "<offers>"
-
-    for product in products:
-        if not product["is_active"]:
-            continue
-
-        for category in categories:
-            if product["category_id"] == category["id"]:
-                if category["is_active"]:
-                    # print(category["id"], '--', category["name"])
-                    break
-        else:
-            continue
-
-        if not product["name"].strip():
-            continue
-
-        if float(product["price"].strip()) <= 0.0:
-            continue
-
-        if not product["image_url"]:
-            continue
-        if not (
-            product["image_url"].strip().startswith("http://")
-            or
-            product["image_url"].strip().startswith("https://")
-        ):
-            continue
-
-        # print('\n\n', product["id"], end=' ')
-        xml += f'<offer id="{product["id"]}" available="{product["stock"]}">'
-
-        xml += f"<url>https://example.test/products/{product['slug']}/</url>"
-
-        price = product["price"].replace(".", ",")
-
-        xml += f"<price>{price}</price>"
-
-        if product["old_price"]:
-            if float(product["old_price"]) != 0.0:
-                if float(product["old_price"]) > float(product["price"]):
-                    xml += f"<oldprice>{product['old_price']}</oldprice>"
-                    print(product["price"], "::", product["old_price"])
-
-        xml += "<currencyId>RUB</currencyId>"
-        xml += f"<categoryId>{product['category_id']}</categoryId>"
-        xml += f"<picture>{product['image_url']}</picture>"
-        xml += f"<name>{product['name']}</name>"
-        if product["description"]:
-            xml += f"<description>{product['description']}</description>"
-        else:
-            print(">>NODESC>>", product["id"])
-        xml += "</offer>"
-
-    xml += "</offers>"
-    xml += "</shop>"
-    xml += "</yml_catalog>"
-
-    return xml
-
-
 if __name__ == "__main__":
     result = build_yml(
         products=PRODUCTS,
         categories=CATEGORIES,
         generated_at=datetime(2026, 6, 18, 12, 0),
     )
-    print(result)
+    # print(result)
